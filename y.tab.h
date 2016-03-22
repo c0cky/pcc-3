0af7d822202e41f232db3b8eec9099ad714cf385
c0cky <camron248@gmail.com>
2016-03-22 15:18:35 -0400

    intial

---
+++ y.tab.h
@@ -0,0 +1,195 @@
+/* A Bison parser, made by GNU Bison 3.0.2.  */
+
+/* Bison interface for Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+#ifndef YY_YY_Y_TAB_H_INCLUDED
+# define YY_YY_Y_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int yydebug;
+#endif
+
+/* Token type.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+  enum yytokentype
+  {
+    IDENTIFIER = 258,
+    INT_CONSTANT = 259,
+    DOUBLE_CONSTANT = 260,
+    STRING_LITERAL = 261,
+    SIZEOF = 262,
+    PTR_OP = 263,
+    INC_OP = 264,
+    DEC_OP = 265,
+    LEFT_OP = 266,
+    RIGHT_OP = 267,
+    LE_OP = 268,
+    GE_OP = 269,
+    EQ_OP = 270,
+    NE_OP = 271,
+    AND_OP = 272,
+    OR_OP = 273,
+    MUL_ASSIGN = 274,
+    DIV_ASSIGN = 275,
+    MOD_ASSIGN = 276,
+    ADD_ASSIGN = 277,
+    SUB_ASSIGN = 278,
+    LEFT_ASSIGN = 279,
+    RIGHT_ASSIGN = 280,
+    AND_ASSIGN = 281,
+    XOR_ASSIGN = 282,
+    OR_ASSIGN = 283,
+    TYPE_NAME = 284,
+    TYPEDEF = 285,
+    EXTERN = 286,
+    STATIC = 287,
+    AUTO = 288,
+    REGISTER = 289,
+    CHAR = 290,
+    SHORT = 291,
+    INT = 292,
+    LONG = 293,
+    SIGNED = 294,
+    UNSIGNED = 295,
+    FLOAT = 296,
+    DOUBLE = 297,
+    CONST = 298,
+    VOLATILE = 299,
+    VOID = 300,
+    STRUCT = 301,
+    UNION = 302,
+    ENUM = 303,
+    ELIPSIS = 304,
+    CASE = 305,
+    DEFAULT = 306,
+    IF = 307,
+    ELSE = 308,
+    SWITCH = 309,
+    WHILE = 310,
+    DO = 311,
+    FOR = 312,
+    GOTO = 313,
+    CONTINUE = 314,
+    BREAK = 315,
+    RETURN = 316,
+    BAD = 317
+  };
+#endif
+/* Tokens.  */
+#define IDENTIFIER 258
+#define INT_CONSTANT 259
+#define DOUBLE_CONSTANT 260
+#define STRING_LITERAL 261
+#define SIZEOF 262
+#define PTR_OP 263
+#define INC_OP 264
+#define DEC_OP 265
+#define LEFT_OP 266
+#define RIGHT_OP 267
+#define LE_OP 268
+#define GE_OP 269
+#define EQ_OP 270
+#define NE_OP 271
+#define AND_OP 272
+#define OR_OP 273
+#define MUL_ASSIGN 274
+#define DIV_ASSIGN 275
+#define MOD_ASSIGN 276
+#define ADD_ASSIGN 277
+#define SUB_ASSIGN 278
+#define LEFT_ASSIGN 279
+#define RIGHT_ASSIGN 280
+#define AND_ASSIGN 281
+#define XOR_ASSIGN 282
+#define OR_ASSIGN 283
+#define TYPE_NAME 284
+#define TYPEDEF 285
+#define EXTERN 286
+#define STATIC 287
+#define AUTO 288
+#define REGISTER 289
+#define CHAR 290
+#define SHORT 291
+#define INT 292
+#define LONG 293
+#define SIGNED 294
+#define UNSIGNED 295
+#define FLOAT 296
+#define DOUBLE 297
+#define CONST 298
+#define VOLATILE 299
+#define VOID 300
+#define STRUCT 301
+#define UNION 302
+#define ENUM 303
+#define ELIPSIS 304
+#define CASE 305
+#define DEFAULT 306
+#define IF 307
+#define ELSE 308
+#define SWITCH 309
+#define WHILE 310
+#define DO 311
+#define FOR 312
+#define GOTO 313
+#define CONTINUE 314
+#define BREAK 315
+#define RETURN 316
+#define BAD 317
+
+/* Value type.  */
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE YYSTYPE;
+union YYSTYPE
+{
+#line 20 "gram.y" /* yacc.c:1909  */
+
+	int	y_int;
+	double	y_double;
+	char *	y_string;
+	
+
+#line 185 "y.tab.h" /* yacc.c:1909  */
+};
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+
+extern YYSTYPE yylval;
+
+int yyparse (void);
+
+#endif /* !YY_YY_Y_TAB_H_INCLUDED  */
