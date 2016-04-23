6ae0d8bcd1e5f0ba64150f4500b4b5abc89fb0bc
aaronpecora <aaron.pecora@gmail.com>
2016-04-23 00:40:04 -0400

    Added test files
    t3.c is the T3L80_ok.c, just what I had it called for the "make run", so sent it
    Don't think there are any updates from the last for any of the  .[ch]'s, but just in case.
    
    I may need the rest of the team to bring us to 100%... 
    Error checking couldn't hurt either...

--- t3.c
+++ t3.c
@@ -4,7 +4,7 @@
 
 int i,j;
 
-int f1(), f2(), f3(), f4(), f5();
+int f1(), f2(), f3(), f4(), f5(), f6(), f7(), f8(), f9();
 
 main()
 {
@@ -13,6 +13,10 @@ main()
     f3();
     f4();
     f5();
+    f6();
+    f7();
+    f8();
+    f9();
 }
 
 int a,b,c;
@@ -94,4 +98,108 @@ f5() {
     x = 1;
     z = nine2();
     print_z();
+}
+
+int print_oops(), print_7(), print_1(), print_2(), print_newline();
+int print_is(), print_awesomest_nl();
+
+f6() {
+
+    a = 0-1;
+    b = 0;
+    c = 1;
+
+    if (a > b) {
+        print_oops();
+    } else {
+        if (b == (c+a))  {
+            print_7();
+            print_1();
+            if (1) print_2();
+            print_newline();
+        } else {
+            print_oops();
+        }
+        print_is();
+    }
+
+    print_awesomest_nl();
+
+}
+
+int print_outer(), print_inner(), print_inner_inner();
+
+f7() {
+
+    a = 0-1;
+
+    while (a > 3) {
+        print_oops();
+    } 
+
+    while (a < 3) {
+
+        print_outer();
+
+        b = 0;
+        while (b < (a+1)) {
+            print_inner();
+       
+            c = 0;
+            while (c < 0) {
+                print_inner_inner();
+                c = c+1;
+            }
+
+            b = b+1;
+        }
+
+        a = a+1;
+    } 
+
+}
+
+int print_looping();
+
+f8() {
+
+    x = 9;
+
+    while (1) {
+        if (x < 0) break;
+        print_looping();
+        x = x - 1;
+    }
+
+    if (x) {
+        x = 0;
+    }
+}
+
+int print_looping_looping(), print_looping_looping_looping();
+
+f9() {
+
+    x = 3;
+
+    while (1) {
+        if (x < 0) break;
+        print_looping();
+        b = 0;
+
+        while (1) {
+            if (b > 1) break;
+            print_looping_looping();
+            c = 8;
+
+            while (1) {
+                if (c > 10) break;
+                print_looping_looping_looping();
+                c = c+1;
+            }
+            b = b + 1;
+        }
+        x = x - 1;
+    }
+
 }
